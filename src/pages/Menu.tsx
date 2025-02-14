85025423989ca287fd50a779313309e06ca98180
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-02-14 22:04:32 +0800

    Added home/details page route

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -19,6 +19,7 @@ import {
     import { Redirect, Route } from 'react-router';
     import Home from './Home';
     import About from './About';
+    import Details from './Details';
   
   const Menu: React.FC = () => {
 
@@ -54,8 +55,10 @@ import {
                     </IonContent>
                 </IonMenu>
                 <IonRouterOutlet id="main">
-                <Route exact path="/it35-lab/app/home" component={Home} />
+                    <Route exact path="/it35-lab/app/home" component={Home} />
+                    <Route exact path="/it35-lab/app/home/details" component={Details} />
                     <Route exact path="/it35-lab/app/about" component={About} />
+                    
                     <Route exact path="/it35-lab/app">
                     <Redirect to="/it35-lab/app/home"/>
                     </Route>
