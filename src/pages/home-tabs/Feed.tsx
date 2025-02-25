11cb1f931c997b24420a7f69bb2eb1527ddc4a5a
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-02-25 14:38:55 +0800

    Added initial content on feed.tsx using card

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,5 +1,10 @@
 import { 
     IonButtons,
+      IonCard,
+      IonCardContent,
+      IonCardHeader,
+      IonCardSubtitle,
+      IonCardTitle,
       IonContent, 
       IonHeader, 
       IonMenuButton, 
@@ -23,13 +28,32 @@ import {
         <div
           style={{
             display: 'flex',
+            flexDirection: 'column',
             alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
+            justifyContent: 'flex-start',
+            minHeight:'100vh',
+            paddingBottom:'20px',
           }}
         >
-          Feed
+          <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Weather Updates</IonCardTitle>
+        <IonCardSubtitle>Manolo Fortich</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Good day ! As of Today February 25, 2025 , Weather is on cloudy sky and expected rain on the evening</IonCardContent>
+    </IonCard>
+    <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
         </div>
+        
         </IonContent>
       </IonPage>
     );
