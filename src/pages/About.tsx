707f86204921b809d980882039a60992d46ab831
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-03-10 21:47:14 +0800

    Added image on About.tsx from facebook image link

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -27,10 +27,10 @@ import {
         </IonHeader>
         <IonContent className='ion-padding'>
         <IonCard className="profile-card">
-        <img className="profile-img" alt="Clifford's profile picture" src='/cliford_profile.jpg' />
+        <img alt="Clifford's profile picture" src='https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-1/480516990_3025784607577266_4596026472426777348_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=103&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeHKkW9zrYpQ6m5taGZI0FEzPpgTOgK1BwQ-mBM6ArUHBHgIcIIt7iJC7egOLwulwg-iwwpnL8mF7UF_gLnqkqg7&_nc_ohc=5nDD3fmMDLEQ7kNvgH3fyHu&_nc_zt=24&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=ARbPqRj9cyeLZG7n3BQuyfc&oh=00_AYFAKh9VfyQN-oQEjf1uzb8uNZhzq4WOG64KjoyD61LZbg&oe=67D4A51B' />
       <IonCardHeader>
-        <IonCardTitle>Profile</IonCardTitle>
-        <IonCardSubtitle>Clifford John B. Lutrago</IonCardSubtitle>
+        <IonCardTitle>Clifford John B. Lutrago</IonCardTitle>
+        <IonCardSubtitle>cjfordmoko</IonCardSubtitle>
       </IonCardHeader>
 
       <IonCardContent>You can't undo past but you can do a better future</IonCardContent>
