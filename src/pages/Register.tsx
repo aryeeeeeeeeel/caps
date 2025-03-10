876489732e5c61895bb365bbc898829cc4461ca5
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-03-10 19:41:23 +0800

    Added Ioninput Email,Password, and Confirm password in Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,6 +2,10 @@ import {
     IonButtons,
       IonContent, 
       IonHeader, 
+      IonInput, 
+      IonInputPasswordToggle, 
+      IonItem, 
+      IonList, 
       IonMenuButton, 
       IonPage, 
       IonTitle, 
@@ -20,6 +24,24 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingTop:'10%' }}>
+        <IonList>
+      <IonItem>
+        <IonInput label="Email" type="email" placeholder="email@domain.com"></IonInput>
+      </IonItem>
+
+      <IonItem>
+      <IonInput type="password" label="Password" value="">
+      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+      </IonInput>
+      </IonItem>
+      <IonItem>
+      <IonInput type="password" label="Confirm Password" value="">
+      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+      </IonInput>
+      </IonItem>
+    </IonList>
+    </div>
         </IonContent>
       </IonPage>
     );
