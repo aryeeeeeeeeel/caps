5dec48255d7bbccf34e511c9aeec99e8c950416f
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-03-10 19:57:55 +0800

    Added alert component in Register.tsx after succesfull registration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,63 +1,81 @@
+import { useState } from 'react';
 import { 
   IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonInput, 
-      IonInputPasswordToggle, 
-      IonItem, 
-      IonList, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Register: React.FC = () => {
-    const navigation = useIonRouter();
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonItem, 
+  IonList, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  useIonRouter,
+  IonAlert
+} from '@ionic/react';
 
-    const doRegister = ()=> {
-      navigation.push('/it35-lab', 'forward','replace');
-  }
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Register</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
+const Register: React.FC = () => {
+  const navigation = useIonRouter();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const doRegister = () => {
+    setIsOpen(true); 
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Register</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingTop:'10%' }}>
-        <IonList>
-      <IonItem>
-        <IonInput label="Email" type="email" placeholder="email@domain.com"></IonInput>
-      </IonItem>
+          <IonList>
+            <IonItem>
+              <IonInput label="Email" type="email" placeholder="email@domain.com"></IonInput>
+            </IonItem>
 
-      <IonItem>
-      <IonInput type="password" label="Password" value="">
-      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-      </IonInput>
-      </IonItem>
-      <IonItem>
-      <IonInput type="password" label="Confirm Password" value="">
-      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-      </IonInput>
-      </IonItem>
-    </IonList>
-    </div>
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-          
-          <IonButton onClick={()=>doRegister()} fill="solid">
+            <IonItem>
+              <IonInput type="password" label="Password">
+                <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+              </IonInput>
+            </IonItem>
+
+            <IonItem>
+              <IonInput type="password" label="Confirm Password">
+                <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+              </IonInput>
+            </IonItem>
+          </IonList>
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <IonButton onClick={doRegister} fill="solid">
             Register
           </IonButton>
-          </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Register;
+        </div>
+        <IonAlert
+          isOpen={isOpen}
+          header="Registration Successful"
+          subHeader="Welcome!"
+          message="You have successfully registered."
+          buttons={[
+            {
+              text: "OK",
+              handler: () => navigation.push('/it35-lab', 'forward', 'replace')
+            }
+          ]}
+          onDidDismiss={() => setIsOpen(false)}
+        />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Register;
