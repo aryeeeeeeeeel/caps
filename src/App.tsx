20cec1de225e2da5a57ae15c1dc9c35892de0785
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-10-24 17:20:55 +0800

    feat(App): update App.tsx with latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,8 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/* Dark mode (system default) */
-import '@ionic/react/css/palettes/dark.system.css';
+/* Dark mode disabled - using consistent color scheme */
+/* import '@ionic/react/css/palettes/dark.system.css'; */
 
 /* Theme variables */
 import './theme/variables.css';
